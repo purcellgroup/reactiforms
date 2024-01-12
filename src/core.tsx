e5c02133724ad0141f149b1f61d4aab490a3f8c5
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-12 12:43:03 +0530

    add documentation for readability

--- src/core.tsx
+++ src/core.tsx
@@ -1,3 +1,4 @@
+// Importing necessary modules from external file
 import React, {
   useState,
   useEffect,
@@ -17,6 +18,7 @@ import {
   DefaultInput,
 } from "./types";
 
+// Default options for an input
 // todo: extract function defs and flatten
 export const createDefaultInputOptions = () => ({
   id: "",
@@ -38,6 +40,7 @@ export const createDefaultInputOptions = () => ({
   debounce: 1300,
 });
 
+// Form class encapsulating form management logic
 export class Form {
   private inputCounter: number;
   private inputMap: Map<string | number, Input>;
@@ -45,16 +48,19 @@ export class Form {
   private FormContext: React.Context<FormContext>;
 
   constructor() {
+    // Initialize form properties
     this.inputCounter = 0;
     this.inputMap = new Map();
     this.subscriberMap = new Map();
     this.FormContext = React.createContext({} as FormContext);
   }
 
+  // Get the form context
   useFormContext = () => {
     return React.useContext(this.FormContext);
   };
 
+  // Register an input in the form
   registerInput = (
     key: number | string | null,
     input: Input
@@ -63,7 +69,7 @@ export class Form {
     this.inputMap.set(inputId, input);
     this.subscriberMap.set(inputId, new Set());
 
-    //unregister input
+    // unregister input
     return {
       newInputId: inputId,
       unregister: (key: number | string) => {
@@ -75,6 +81,7 @@ export class Form {
     };
   };
 
+  // Update an input in the form
   updateInput = (key: string | number, newInput: Input) => {
     if (!newInput || key === null)
       throw Error("Invalid data while updating inputMap");
@@ -83,6 +90,7 @@ export class Form {
   };
 
   //!! TODO: test
+  // Subscribe to changes in an input
   subscribeToInput = (
     key: string | number,
     dispatch: React.Dispatch<Input>
@@ -93,8 +101,8 @@ export class Form {
       throw new Error("Subscribe Error: subscriber map failed");
 
     subscriberSet.add(dispatch);
-
-    // unsubscribe to input
+    
+    // Unsubscribe to input changes
     return (dispatch: React.Dispatch<Input>) => {
       const fail = subscriberSet.delete(dispatch);
       if (fail) throw new Error("Failed unsubscribing from input.");
@@ -102,6 +110,7 @@ export class Form {
   };
 
   //!! TODO: test
+  // Broadcast changes to all subscribers of an input
   broadcastToSubscribers = (key: string | number) => {
     const inputSet = this.subscriberMap.get(key);
     const inputVal = this.inputMap.get(key);
@@ -112,6 +121,7 @@ export class Form {
     inputSet.forEach((dispatch) => dispatch(inputVal));
   };
 
+  // Reset all inputs in the form
   resetForm = () => {
     this.inputMap.forEach((input: Input) => {
       if (input.setter)
@@ -123,6 +133,7 @@ export class Form {
     });
   };
 
+  // Get the current values of all inputs in the form
   getFormValues = () => {
     return Array.from(this.inputMap).reduce(
       (acc: FormValues, [id, input]): FormValues => {
@@ -133,6 +144,7 @@ export class Form {
     );
   };
 
+  // Get all inputs in the form
   getFormInputs = (): FormInputs => {
     if (this.inputMap.size) {
       return Object.fromEntries(this.inputMap);
@@ -145,16 +157,19 @@ export class Form {
   };
 
   //!! TODO: test
+  // Get an input by ID
   getInput = (id: any): Input | undefined => {
     return this.inputMap.get(id);
   };
 
+  // Check if the form is valid based on input validations
   isValid = () => {
     return Array.from(this.inputMap.entries()).every(
       ([, input]) => input.isValid
     );
   };
 
+  // React component for the form
   Form = (props: React.DOMAttributes<any>) => {
     const C = this.FormContext;
     return (
@@ -175,6 +190,7 @@ export class Form {
 
   // TODO: adjust input factory for new form instance
   // todo: needs form properties and input register
+  // React component for an input in the form
   Input = (props: Input) => {
     const _form = this.useFormContext();
     //props needs to be internally stable
@@ -292,6 +308,7 @@ export class Form {
     );
   };
 
+  // Create a public form instance
   createPublicForm = (): FormInstance => {
     return {
       resetForm: this.resetForm,
@@ -305,23 +322,29 @@ export class Form {
   };
 }
 
+// Factory function to create a form instance
 export function createForm(): FormInstance {
   return new Form().createPublicForm();
 }
 
+// Check if a given value is a function
 export const isFunction = (fn: any): fn is Function => {
   if (typeof fn === "function") return true;
   console.error(new Error("Optional handlers must be functions."));
   return false;
 };
 
-// below is infra for possible larger form pub/sub if necessary
-// copy of store for mutations
+// Infrastructure for possible larger form pub/sub if necessary
+// Copy of store for mutations
+// A map to store instances of forms
 export let FORM_STORE_INSTANCES: Map<number, Form> = new Map();
+// A copy of the form store for mutations
 let NEXT_FORM_STORE_INSTANCES: Map<number, Form> = FORM_STORE_INSTANCES;
 
+// Counter to generate unique form IDs
 let FORM_COUNTER = 0;
 
+// Function to unsubscribe a form with a given formId
 const UNSUB_FORM = (formId: number) => {
   GEN_MUTABLE_STORE();
 
@@ -329,12 +352,14 @@ const UNSUB_FORM = (formId: number) => {
   FORM_STORE_INSTANCES = NEXT_FORM_STORE_INSTANCES;
 };
 
+// Function to subscribe a form with a given formId and form instance
 const SUB_FORM = (formId: number, form: Form) => {
   GEN_MUTABLE_STORE();
   NEXT_FORM_STORE_INSTANCES.set(formId, form);
   FORM_STORE_INSTANCES = NEXT_FORM_STORE_INSTANCES;
 };
 
+// Function to generate a mutable store to avoid rerenders
 const GEN_MUTABLE_STORE = () => {
   if (NEXT_FORM_STORE_INSTANCES === FORM_STORE_INSTANCES) {
     NEXT_FORM_STORE_INSTANCES = new Map();
@@ -344,11 +369,13 @@ const GEN_MUTABLE_STORE = () => {
   }
 };
 
+// Function to generate a unique form ID
 const GEN_FORM_ID = () => {
   ++FORM_COUNTER;
   return FORM_COUNTER;
 };
 
+// Function to generate form options with default values
 const GEN_FORM_OPTIONS = (
   config: Record<string, any>
 ): DefaultForm & Record<string, any> => ({
@@ -362,15 +389,16 @@ const GEN_FORM_OPTIONS = (
   ...config,
 });
 
+// Function to create default form options
 const createDefaultFormOptions = () => ({
-  //react spinner component
+  // React spinner component
   requireSpinner: false,
   spinner: null,
-  // todo form state: changes, dirty fields, touched fields
-  // todo: input overloads. these are functions applied to all inputs
-  // todo: in form by default
+  // Todo: form state: changes, dirty fields, touched fields
+  // Todo: input overloads. These are functions applied to all inputs
+  // Todo: in form by default
   inputUtils: {},
-  // todo: submit suspense flag
+  // Todo: submit suspense flag
   suspense: false,
   spinnerTimeout: null,
   onSubmit: () => {
